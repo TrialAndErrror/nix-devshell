78e7da0e60c621bbfe4d506979b997138968be03
Trial And Errror <coding@trialanderrror.com>
2023-05-28 17:56:01 -0400

    updating to separate configs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    wade-shell.url = "github:TrialAndErrror/nix-devshell";
   };
 
-  outputs = { self, nixpkgs, wade-shell }: {
-      packages.x86_64-linux.default = self.wade-shell;
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages = flake-utils.lib.flattenTree {
+        inherit (pkgs) hello;
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs =  [ 
+          pkgs.git
+          pkgs.python3
+          pkgs.sublime
+          pkgs.firefox
+          pkgs.neovim
+          pkgs.ripgrep
+          pkgs.lazygit
+          pkgs.gh
+        ];
+      };
+    });
 }
