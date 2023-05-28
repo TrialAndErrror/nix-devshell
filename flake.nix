be220ac144a2a351a9988716258a998c292fdbde
Trial And Errror <coding@trialanderrror.com>
2023-05-28 19:11:49 -0400

    Running in a flake perhaps?

--- flake.nix
+++ flake.nix
@@ -1,33 +1,15 @@
 {
   description = "Wade's devshell flake";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          apps.${system}.default = {
-            type = "app";
-            program = "ECHO Hello World!";
-          };
-
-          # devShells.default = nixpkgs.mkShell {
-          #   buildInputs =  [ 
-          #     nixpkgs.git
-          #     nixpkgs.python3
-          #     nixpkgs.sublime
-          #     nixpkgs.firefox
-          #     nixpkgs.neovim
-          #     nixpkgs.ripgrep
-          #     nixpkgs.lazygit
-          #     nixpkgs.gh
-          #   ];
-          # };
+          devShells.default = import ./default.nix { inherit pkgs; };
         }
       );
-    }
+}
 
