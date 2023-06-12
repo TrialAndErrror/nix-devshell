d8119fd1cddccbd23394c7c568b7429a6afe8c18
Wade Green <thewadegreen@gmail.com>
2023-06-12 19:54:22 -0400

    formatting

--- flake.nix
+++ flake.nix
@@ -9,55 +9,55 @@
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           devShells.default = pkgs.mkShell { 
-	  	buildInputs = with pkgs; [ 
-			git
-		        gh
-		        lazygit
+  	  	    buildInputs = with pkgs; [ 
+              git
+      		    gh
+              lazygit
 
-		        python3
-		        rustup
-		        nodejs
-		        yarn
+              python3
+              rustup
+              nodejs
+              yarn
 
-		        black
-		        nodePackages.prettier
-		      
-		        # neovim
-		        tmux
-		        ripgrep
-		        lazydocker
-		]; 
+              black
+              nodePackages.prettier
+            
+              # neovim
+              tmux
+              ripgrep
+              lazydocker
+      		  ]; 
 
-	    shellHook = ''
-	      echo " _____     ______     __   __   ______     __  __     ______     __         __        ";
-	      echo "/\  __-.  /\  ___\   /\ \ / /  /\  ___\   /\ \_\ \   /\  ___\   /\ \       /\ \       ";
-	      echo "\ \ \/\ \ \ \  __\   \ \ \'/   \ \___  \  \ \  __ \  \ \  __\   \ \ \____  \ \ \____  ";
-	      echo " \ \____-  \ \_____\  \ \__|    \/\_____\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\ ";
-	      echo "  \/____/   \/_____/   \/_/      \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_____/ ";
-	      echo "                                                                                      ";
-	      
-	      echo "
-	      git
-	      github cli (gh)
-	      lazygit
-	      
-	      python3 (3.10.11)
-	      black
+            shellHook = ''
+              echo " _____     ______     __   __   ______     __  __     ______     __         __        ";
+              echo "/\  __-.  /\  ___\   /\ \ / /  /\  ___\   /\ \_\ \   /\  ___\   /\ \       /\ \       ";
+              echo "\ \ \/\ \ \ \  __\   \ \ \'/   \ \___  \  \ \  __ \  \ \  __\   \ \ \____  \ \ \____  ";
+              echo " \ \____-  \ \_____\  \ \__|    \/\_____\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\ ";
+              echo "  \/____/   \/_____/   \/_/      \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_____/ ";
+              echo "                                                                                      ";
+              
+              echo "
+              git
+              github cli (gh)
+              lazygit
+              
+              python3 (3.10.11)
+              black
 
-	      rustup
-	      
-	      nodejs
-	      yarn
-	      prettier
-	      
-	      nvim (vanilla neovim)
-	      tmux
-	      ripgrep
-	      lazydocker
-	      "
-	    '';
-	};
-	}
+              rustup
+              
+              nodejs
+              yarn
+              prettier
+              
+              nvim (vanilla neovim)
+              tmux
+              ripgrep
+              lazydocker
+              "
+            '';
+          };
+	       }
       );
 }
 
