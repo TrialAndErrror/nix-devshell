79316ebc45522bc48e633bd7f70a1833e4b54bc1
Trial And Errror <coding@trialanderrror.com>
2023-08-06 12:02:02 -0400

    update and add rust tools

--- flake.nix
+++ flake.nix
@@ -20,22 +20,45 @@
     devShells.${system}.default =
       pkgs.mkShell
       {
-        buildInputs = [
-          pkgs.git
-          pkgs.gh
-          pkgs.lazygit
-          pkgs.python3
-          pkgs.rustup
-          pkgs.nodejs
-          pkgs.yarn
-          pkgs.black
-          pkgs.nodePackages.prettier
-          pkgs.tmux
-          pkgs.ripgrep
-          pkgs.lazydocker
+        buildInputs = with pkgs; [
+          git
+          gh
+
+          # Python tools
+          python3
+          black
+          poetry
+
+          # Rust tools
+          rustup
+          cargo
+          bacon # rust debugging
+
+          # JS tools
+          nodejs
+          yarn
+          nodePackages.prettier
+
+          # Command line tools
+          exa # better ls
+          du-dust # disk usage
+          bat # syntax highlighting on cat
+
+          zellij # better tmux
+          mprocs # monitor long running processes
+
+          ripgrep # fast search
+
+          lazydocker
+          gitui # lazygit replacement
+
+          # include neovim-flake
           editor.packages.x86_64-linux.default
           # editor.devShells.default
         ];
+        shellHook = ''
+          echo "Hello, this worked!";
+        '';
       };
   };
 }
